88b9d9b7ef26d5c9c85fcbce9d48e0ec0ccc7536
skotani-db <skotani-db>
2025-04-06 10:28:52 +0000

    add YellowTaxis_append.csv

--- db-notebooks/Chapter-03/00 - Chapter Initialization.py
+++ db-notebooks/Chapter-03/00 - Chapter Initialization.py
@@ -93,37 +93,11 @@ dbutils.fs.rm("/mnt/datalake/book/chapter03/YellowTaxis_append.csv", recurse=Tru
 
 # COMMAND ----------
 
-import pandas as pd
+try:
+    dbutils.fs.cp('dbfs:/FileStore/tables/data/YellowTaxis_append.csv','dbfs:/mnt/datalake/book/chapter03/YellowTaxis_append.csv')
+except:
+    print("ファイルが存在しません。dbfs:/FileStore/tables/dataへYellowTaxis_append.csvファイルをアップロードしてください。")
 
-# データの作成
-data = {
-   'RideId': [9999996, 9999997, 9999998, 9999999],
-   'VendorId': [1, 1, 1, 1],
-   'PickupTime': ['2019-01-01T00:00:00.000Z', '2019-01-01T00:00:00.000Z', '2019-01-01T00:00:00.000Z', '2019-01-01T00:00:00.000Z'],
-   'DropTime': ['2022-01-01T00:13:13.000Z', '2022-01-01T00:09:21.000Z', '2022-01-01T00:09:15.000Z', '2022-01-01T00:10:01.000Z'],
-   'PickupLocationId': [170, 161, 141, 161],
-   'DropLocationId': [140, 68, 170, 68],
-   'CabNumber': ['TAC399', 'T489328C', 'T509308C', 'VG354'],
-   'DriverLicenseNumber': [5131685, 5076150, 5067782, 5012911],
-   'PassengerCount': [1, 1, 0, 2],
-   'TripDistance': [2.9, 1.1, 1.7, 2.86],
-   'RateCodeId': [1, 1, 1, 1],
-   'PaymentType': [1, 2, 1, 1],
-   'TotalAmount': [15.3, 9.8, 12.35, 18.17],
-   'FareAmount': [13.0, 8.5, 9.0, 14.5],
-   'Extra': [0.5, 0.5, 0.5, 0.5],
-   'MtaTax': [0.5, 0.5, 0.5, 0.5],
-   'TipAmount': [1.0, 0.0, 2.05, 2.37],
-   'TollsAmount': [0.0, 0.0, 0.0, 0.0],
-   'ImprovementSurcharge': [0.3, 0.3, 0.3, 0.3]
-}
+# COMMAND ----------
 
-# データフレーム作成
-df = pd.DataFrame(data)
 
-# データフレーム作成
-df = spark.createDataFrame(df)
-# CSVファイルに出力
-df.write.csv("/mnt/datalake/book/chapter03/YellowTaxis_append.csv", header=True, mode="overwrite")
-
-print("CSVファイルが正常に出力されました: /mnt/datalake/book/chapter03/YellowTaxis_append.csv")
